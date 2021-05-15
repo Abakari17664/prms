48ee0661b103126f468965e1433c9c93bcd5b02f
ABUBAKAR BAKARI <abakari4na@gmail.com>
2021-05-15 07:37:43 +0100

    added video page and video.html file at www dir

--- prms/hooks.py
+++ prms/hooks.py
@@ -49,9 +49,9 @@ web_include_css = "/assets/prms/css/prms.css"
 # application home page (will override Website Settings)
 # home_page = "login"
 
-# website user home page (by Role)
+# # website user home page (by Role)
 # role_home_page = {
-#	"Role": "home_page"
+# 	"PRMS": "/app/prms"
 # }
 
 # Generators
---
+++ prms/prms/page/project_videos/project_videos.js
@@ -0,0 +1,26 @@
+frappe.pages['project-videos'].on_page_load = function(wrapper) {
+	var page = frappe.ui.make_app_page({
+		parent: wrapper,
+		title: 'Project Videos test',
+		single_column: true
+	});
+}
+
+// frappe.pages['project-videos'].on_page_load = function(wrapper) {
+// 	new MyPage(wrapper);
+// }
+// MyPage = Class.extend({
+// init: function(wrapper) {
+//    this.page = frappe.ui.make_app_page({
+// 	   parent: wrapper,
+// 	   title: 'PRMS',
+// 	   single_column: true
+//    });
+//    this.make();
+// },
+// make: function() {
+//    $(frappe.render_template("project_videos", this)).appendTo(this.page.main);
+// }
+// })
+
+// https://discuss.erpnext.com/t/custom-page-how-to-connect-html-template/42111/6
